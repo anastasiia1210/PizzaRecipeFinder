8a9fda9abed65a4eb259dad5c2600c15f24d717b
Svitlana-Marchenko <sveta25022004@gmail.com>
2024-06-21 12:25:55 +0300

    feature: add toppings model

---
+++ PizzaRecipeFinder/Resources/PizzaTypeIDs.swift
@@ -0,0 +1,19 @@
+//
+//  PizzaTypeIDs.swift
+//  PizzaRecipeFinder
+//
+//  Created by Svitlana on 19.06.2024.
+//
+
+import Foundation
+
+let pizzaIDMapping: [String: Int] = [
+    "capricciosa": 1,
+    "diavola": 2,
+    "margherita": 3,
+    "marinara": 4,
+    "multiple": 5,
+    "ortolana": 6,
+    "quattro-formaggi": 7,
+    "other": 0
+]
